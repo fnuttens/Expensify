8eea4da97694da04c65bda1c6f556b719b130649
Florent Nuttens <fnuttens@gmail.com>
2019-02-06 19:49:25 +0100

    feat(webpack): configured production mode

--- webpack.config.js
+++ webpack.config.js
@@ -2,30 +2,34 @@ const path = require('path');
 
 const publicPath = path.resolve(__dirname, 'public');
 
-module.exports = {
-  entry: './src/app.js',
-  output: {
-    path: publicPath,
-    filename: 'bundle.js'
-  },
-  module: {
-    rules: [{
-      loader: 'babel-loader',
-      test: /^.*\.js$/,
-      exclude: /node_modules/
-    }, {
-      test: /^.*\.s?css$/,
-      use: [
-        'style-loader',
-        'css-loader',
-        'sass-loader'
-      ]
-    }]
-  },
-  mode: 'development',
-  devtool: 'cheap-module-eval-source-map',
-  devServer: {
-    contentBase: publicPath,
-    historyApiFallback: true
-  }
+module.exports = (env) => {
+  const isProduction = env === 'production';
+
+  return {
+    entry: './src/app.js',
+    output: {
+      path: publicPath,
+      filename: 'bundle.js'
+    },
+    module: {
+      rules: [{
+        loader: 'babel-loader',
+        test: /^.*\.js$/,
+        exclude: /node_modules/
+      }, {
+        test: /^.*\.s?css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      }]
+    },
+    mode: 'development',
+    devtool: isProduction ? 'source-map' : 'cheap-module-eval-source-map',
+    devServer: {
+      contentBase: publicPath,
+      historyApiFallback: true
+    }
+  };
 };
