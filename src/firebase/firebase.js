39e3944e1bc6bf4b771450d4bce860d589406c9a
Florent Nuttens <fnuttens@gmail.com>
2019-02-12 23:04:43 +0100

    chore: setup firebase

---
+++ src/firebase/firebase.js
@@ -0,0 +1,12 @@
+import * as firebase from 'firebase';
+
+const config = {
+  apiKey: "AIzaSyDQ920Rs3zFYW_hvmTID8L2BHojeDOZ8-M",
+  authDomain: "expensify-69604.firebaseapp.com",
+  databaseURL: "https://expensify-69604.firebaseio.com",
+  projectId: "expensify-69604",
+  storageBucket: "expensify-69604.appspot.com",
+  messagingSenderId: "378223131122"
+};
+
+firebase.initializeApp(config);
